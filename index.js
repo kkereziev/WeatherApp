46cbcfb833232daf3ef17c1fb39ff2b7542944cc
krumeto97 <krumeto97@abv.bg>
2020-05-03 11:43:34 +0300

    The application is done.

--- index.js
+++ index.js
@@ -2,77 +2,85 @@
 const form = document.querySelector(".top-banner form");
 const input = document.querySelector(".top-banner input");
 const msg = document.querySelector(".top-banner .msg");
-const list = document.querySelector(".ajax-section .cities");
+const documentList = document.querySelector(".ajax-section .cities");
 const apiKey = "4d8fb5b93d4af21d66a2948710284366";
 
-form.addEventListener("submit", e => {
+window.addEventListener("load", () => {
+    let long;
+    let lat;
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(position => {
+            long = position.coords.longitude;
+            lat = position.coords.latitude;
+            const url = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${long}&appid=4d8fb5b93d4af21d66a2948710284366&units=metric`;
+            fetch(url)
+                .then((response) => response.json())
+                .then((data) => {
+                    const {
+                        list, //vzima temperatura, //vzima discription za vremeto i ikonka
+                        city, //vzima ime na grad i vzima abreviatura na grada
+                    } = data;
+                    const neededDays = [];
+                    neededDays.push(list[0]);
+                    let wantedHour = timeConverter(list[0]["dt"]);
+                    for (let i = 1; i < list.length - 1; i++) {
+                        let currentDateOfRec = timeConverter(list[i]["dt"]);
+                        if (wantedHour.hour === currentDateOfRec.hour) {
+                            neededDays.push(list[i]);
+                        }
+                    }
+                    for (let i = 0; i < neededDays.length; i++) {
+                        const icon = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${neededDays[i].weather[0]["icon"]}.svg`;
+                        let li = document.createElement("li");
+                        li.classList.add("city");
+                        let a = createInnerHTMLForTag(neededDays[i], icon, city);
+                        li.innerHTML = a;
+                        documentList.appendChild(li);
+                    }
+                })
+                .catch(() => {
+                    msg.textContent = "Please search for a valid city 😩";
+                });
+
+            msg.textContent = "";
+        });
+
+    }
+});
+
+
+
+
+form.addEventListener("submit", (e) => {
     e.preventDefault();
     let inputVal = input.value;
-    //check if there's already a city
-    const listItems = list.querySelectorAll(".ajax-section .city");
-    const listItemsArray = Array.from(listItems);
-    let div = document.querySelector(".container .cities");
-    div.innerHTML = "";
-    // if (listItemsArray.length > 0) {
-    //     const filteredArray = listItemsArray.filter(el => {
-    //         let content = "";
-    //         athens,gr
-    //         if (inputVal.includes(",")) {
-    //             athens,grrrrrr->invalid country code, so we keep only the first part of inputVal
-    //             if (inputVal.split(",")[1].length > 2) {
-    //                 inputVal = inputVal.split(",")[0];
-    //                 content = el
-    //                     .querySelector(".city-name span")
-    //                     .textContent.toLowerCase();
-    //             } else {
-    //                 content = el.querySelector(".city-name").dataset.name.toLowerCase();
-    //             }
-    //         } else {
-    //             athens
-    //             content = el.querySelector(".city-name span").textContent.toLowerCase();
-    //         }
-    //         return content == inputVal.toLowerCase();
-    //     });
-
-    //     if (filteredArray.length > 0) {
-    //         msg.textContent = `You already know the weather for ${
-    //     filteredArray[0].querySelector(".city-name span").textContent
-    //   } ...otherwise be more specific by providing the country code as well 😉`;
-    //         form.reset();
-    //         input.focus();
-    //         return;
-    //     }
-    // }
-
-    //ajax here
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`;
-
+    let divCities = document.querySelector(".container .cities");
+    divCities.innerHTML = "";
+    const url = `https://api.openweathermap.org/data/2.5/forecast?q=${inputVal}&appid=4d8fb5b93d4af21d66a2948710284366&units=metric`;
     fetch(url)
-        .then(response => response.json())
-        .then(data => {
+        .then((response) => response.json())
+        .then((data) => {
             const {
-                main,
-                name,
-                sys,
-                weather
+                list, //vzima temperatura, //vzima discription za vremeto i ikonka
+                city, //vzima ime na grad i vzima abreviatura na grada
             } = data;
-            const icon = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${
-        weather[0]["icon"]}.svg`;
-            const li = document.createElement("li");
-            li.classList.add("city");
-            const markup = `
-        <h2 class="city-name" data-name="${name},${sys.country}">
-          <span>${name}</span>
-          <sup>${sys.country}</sup>
-        </h2>
-        <div class="city-temp">${Math.round(main.temp)}<sup>°C</sup></div>
-        <figure>
-          <img class="city-icon" src="${icon}" alt="${
-        weather[0]["description"]}">
-          <figcaption>${weather[0]["description"]}</figcaption>
-        </figure>`;
-            li.innerHTML = markup;
-            list.appendChild(li);
+            const neededDays = [];
+            neededDays.push(list[0]);
+            let wantedHour = timeConverter(list[0]["dt"]);
+            for (let i = 1; i < list.length - 1; i++) {
+                let currentDateOfRec = timeConverter(list[i]["dt"]);
+                if (wantedHour.hour === currentDateOfRec.hour) {
+                    neededDays.push(list[i]);
+                }
+            }
+            for (let i = 0; i < neededDays.length; i++) {
+                const icon = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${neededDays[i].weather[0]["icon"]}.svg`;
+                let li = document.createElement("li");
+                li.classList.add("city");
+                let a = createInnerHTMLForTag(neededDays[i], icon, city);
+                li.innerHTML = a;
+                documentList.appendChild(li);
+            }
         })
         .catch(() => {
             msg.textContent = "Please search for a valid city 😩";
@@ -82,3 +90,62 @@ form.addEventListener("submit", e => {
     form.reset();
     input.focus();
 });
+
+function createInnerHTMLForTag(we, icon, city) {
+    return `<h2 class="city-name" data-name="${city.name},${city.country}">
+          <span>${city.name}</span>
+          <sup>${city.country}</sup>
+          <span>${we["dt_txt"]}</span>
+        </h2>
+        <div class="city-temp">${Math.round(we.main.temp)}<sup>°C</sup></div>
+        <figure>
+          <img class="city-icon" src="${icon}" alt="${
+    we.weather[0]["description"]
+  }">
+          <figcaption>${we.weather[0]["description"]}</figcaption>
+        </figure>`;
+}
+
+function timeConverter(UNIX_timestamp) {
+    const a = new Date(Number(UNIX_timestamp) * 1000);
+    const months = [
+        "Jan",
+        "Feb",
+        "Mar",
+        "Apr",
+        "May",
+        "Jun",
+        "Jul",
+        "Aug",
+        "Sep",
+        "Oct",
+        "Nov",
+        "Dec",
+    ];
+    const year = a.getFullYear();
+    const month = months[a.getMonth()];
+    const date = a.getDate();
+    const hour = a.getHours() - 3;
+    const min = a.getMinutes();
+    const sec = a.getSeconds();
+    const day = a.getDay();
+    return (time = {
+        year,
+        month,
+        date,
+        day: daysOfWeek[day],
+        hour,
+        min,
+        sec,
+    });
+}
+
+const daysOfWeek = {
+    0: "Sunday",
+    1: "Monday",
+    2: "Tuesday",
+    3: "Wednesday",
+    4: "Thursday",
+    5: "Friday",
+    6: "Saturday",
+};
