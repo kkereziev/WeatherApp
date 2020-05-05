1f65acd501f83d69d93f4c78a6f70f3a25b4ca3c
krumeto97 <krumeto97@abv.bg>
2020-05-05 23:13:07 +0300

    refactor

--- index.js
+++ index.js
@@ -32,21 +32,6 @@ form.addEventListener("submit", (e) => {
   input.focus();
 });
 
-function createInnerHTMLForTag(we, icon, city) {
-  return `<h2 class="city-name" data-name="${city.name},${city.country}">
-          <span>${city.name}</span> 
-          <sup>${city.country}</sup> 
-          <span>${we["dt_txt"]}</span> 
-        </h2>
-        <div class="city-temp">${Math.round(we.main.temp)}<sup>°C</sup></div> 
-        <figure>
-          <img class="city-icon" src="${icon}" alt="${
-    we.weather[0]["description"] //ikonka i deskripshuna na vremeto
-  }">
-          <figcaption>${we.weather[0]["description"]}</figcaption>
-        </figure>`;
-}
-
 function timeConverter(UNIX_timestamp) {
   const a = new Date(Number(UNIX_timestamp) * 1000);
   const months = [
@@ -101,25 +86,47 @@ function fetchUrl(url) {
       } = data;
       const wantedHour = timeConverter(list[0]["dt"]);
 
-      const neededDays = list.reduce((a, b, i) => {
-        i++;
-        let currentDateOfRec = timeConverter(b["dt"]);
-        if (wantedHour.hour === currentDateOfRec.hour) {
-          a.push(b);
-        }
-        return a;
-      }, []);
+      const neededDays = getNeededDays(list, wantedHour);
 
       neededDays.map((x) => {
-        const icon = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${x.weather[0]["icon"]}.svg`; //ikonki za vremeto
-        let li = document.createElement("li");
-        li.classList.add("city");
-        let a = createInnerHTMLForTag(x, icon, city); // fuknkciq za generirane na html-a na otdelnite tabcheta s vremeto
-        li.innerHTML = a;
-        documentList.appendChild(li);
+        createForms(x, city);
       });
     })
     .catch(() => {
       msg.textContent = "Please search for a valid city 😩";
     });
+}
+
+function getNeededDays(list, wantedHour) {
+  return list.reduce((a, b) => {
+    let currentDateOfRec = timeConverter(b["dt"]);
+    if (wantedHour.hour === currentDateOfRec.hour) {
+      a.push(b);
+    }
+    return a;
+  }, []);
+}
+
+function createForms(x, city) {
+  const icon = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${x.weather[0]["icon"]}.svg`; //ikonki za vremeto
+  let li = document.createElement("li");
+  li.classList.add("city");
+  let a = createInnerHTMLForTag(x, icon, city); // fuknkciq za generirane na html-a na otdelnite tabcheta s vremeto
+  li.innerHTML = a;
+  documentList.appendChild(li);
+}
+
+function createInnerHTMLForTag(we, icon, city) {
+  return `<h2 class="city-name" data-name="${city.name},${city.country}">
+          <span>${city.name}</span> 
+          <sup>${city.country}</sup> 
+          <span>${we["dt_txt"]}</span> 
+        </h2>
+        <div class="city-temp">${Math.round(we.main.temp)}<sup>°C</sup></div> 
+        <figure>
+          <img class="city-icon" src="${icon}" alt="${
+    we.weather[0]["description"] //ikonka i deskripshuna na vremeto
+  }">
+          <figcaption>${we.weather[0]["description"]}</figcaption>
+        </figure>`;
 }
